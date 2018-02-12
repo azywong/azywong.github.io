0c2c2062d4d8f5cb6efaca681b38867f0141727b
azywong <azywong@gmail.com>
2018-02-12 14:18:40 -0800

    *bar chart working again with non static axis *changed script src for index files *added data folder

--- dataviz/a2/bar-chart.js
+++ dataviz/a2/bar-chart.js
@@ -1,68 +1,71 @@
 var table;
 var tableObject;
 var data = {};
+var number_entries;
+var max_value;
+var HEIGHT = 600;
+var COLUMN = 29;
 
 function preload() {
-	table = loadTable('music.csv', 'csv', 'header');
+	table = loadTable('data/music.csv', 'csv');
 }
 
 function setup() {
 	createCanvas(1000, 800);
 	tableObject = table.getObject();
 
-	// format data
-	for (var key in tableObject) {
-		var song = tableObject[key];
-		var terms = song.terms;
-		if (terms in data) {
-			data[terms] += 1;
+	for (var r = 1; r < table.getRowCount(); r++) {
+		var cvalue = table.getString(r, COLUMN);
+		if (cvalue in data) {
+			data[cvalue] += 1;
 		} else {
-			data[terms] = 1;
+			data[cvalue] = 1;
 		}
-	};
+ 	}
+
 
 	// find range
-	var max_value = 0;
-	var number_entries = 10;
-	for (var genre in data) {
-		if (data[genre] > max_value) {
-			max_value = data[genre];
+	max_value = 0;
+	number_entries= 10;
+	for (var category in data) {
+		if (data[category] > max_value) {
+			max_value = data[category];
 		}
-	}
-
-	// make scale
-	for(var i = 0; i <= max_value + 40; i += 40) {
-		stroke('#E5E5E5');
-		line(50, 600-i, 78*(number_entries + 1), 600-i);
-		textSize(11);
-		fill('#E5E5E5');
-		textAlign(RIGHT);
-		text(i, 45, 600-i);
 	}
 
 }
 
 function draw() {
 
+	// make scale
+	for(var i = 0; i <= max_value + 40; i += 40) {
+		stroke('#E5E5E5');
+		line(50, HEIGHT-i, 78*(number_entries + 1), HEIGHT-i);
+		textSize(11);
+		fill('#E5E5E5');
+		textAlign(RIGHT);
+		text(i, 45, HEIGHT-i);
+	}
+
 	// draw bars
 	var x = 50;
 	var count = 0;
-	for (var genre in data) {
-		var total = data[genre];
+	for (var category in data) {
+		var total = data[category];
 		if (count < 10) {
 			noStroke();
 			fill('#5FA0C6');
-			rect(x, 600-(total), 75, total);
-			if(mouseX >= x && mouseX <= x+75 && mouseY >= 600-(total) && mouseY <= 600) {
+			rect(x, HEIGHT-(total), 75, total);
+			if(mouseX >= x && mouseX <= x+75 && mouseY >= HEIGHT-(total) && mouseY <= HEIGHT) {
 				textSize(11);
 				fill('#2E4E60');
 				textAlign(CENTER);
-				text(total, x, 600-total/2, 75);
+				text(total, x, HEIGHT-total/2, 75);
 			}
 			textSize(11);
-			fill('#11100E');
+			fill('#2E4E60');
 			textAlign(CENTER);
-			text(genre, x, 615, 75);
+			text(category, x, HEIGHT + 15, 75);
 			x += 78;
 		} else {
 			break;
@@ -70,10 +73,18 @@ function draw() {
 		count++;
 	}
 
-	// draw title
-	fill('#11100E');
+
+	// label x-axis
+	textSize(15);
+	fill('#E5E5E5');
+	textAlign(RIGHT);
+	text("count", 45, HEIGHT - max_value - 40);
+
+	// label y-axis
+	textSize(15);
+	fill('#E5E5E5');
 	textAlign(CENTER);
-	textSize(32);
-	text('various music genres by count of songs', 0, 50, x);
+	text(table.getString(0, COLUMN), 50, HEIGHT + 50, 78*(number_entries + 1));
+
 
 }
